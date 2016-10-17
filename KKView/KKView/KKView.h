a0aad3469b3ffdddf1c6912474ffa510cae34baa
zhang hailong <hailongz@qq.com>
2016-10-17 13:06:24 +0800

    update

---
+++ KKView/KKView/KKView.h
@@ -0,0 +1,19 @@
+//
+//  KKView.h
+//  KKView
+//
+//  Created by zhanghailong on 2016/10/17.
+//  Copyright © 2016年 kkserver.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKView.
+FOUNDATION_EXPORT double KKViewVersionNumber;
+
+//! Project version string for KKView.
+FOUNDATION_EXPORT const unsigned char KKViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKView/PublicHeader.h>
+
+
